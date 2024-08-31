daf48cad3820fc8f120ff3750b4172398f15615a
Dipankar Shaw <dipankarshaw@microsoft.com>
2024-09-01 01:31:25 +0530

    added code for regex matching

--- tests/test_device_version.py
+++ tests/test_device_version.py
@@ -9,9 +9,12 @@ def load_device_versions(path):
 
 def test_device_version(device_connections):
     device_versions = load_device_versions(path="tests/device_versions.yaml")
-    # device_pattern = load_device_versions(path="tests/device_pattern_version.yaml")
-    # print(device_pattern)
+    device_pattern = load_device_versions(path="tests/device_pattern_version.yaml")
+    print(device_pattern)
     for device in device_connections:
         output = device["connection"].send_command("show version")
+        match = re.match(device_pattern.get(device["device_type"]), output, re.MULTILINE)
+        if match:
+            print(match.group(1))
         expected_version = device_versions.get(device["device_type"])
         assert expected_version in output, f"Expected version {expected_version}, but got {output}"
