94a7118c51d221256ae6863df0a27fe47a4e0b66
Dipankar Shaw <dipankarshaw@microsoft.com>
2024-09-01 01:23:24 +0530

    Removed sec from storage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,20 @@
 import pytest
+import os
 from src.device_connector import load_device_config, connect_to_device
 
 @pytest.fixture(scope="module")
 def device_connections():
     devices = load_device_config('src/config.yaml')
     connections = []
+    user_name = os.getenv('USERNAME')
+    pass_word = os.getenv('PASSWORD')
     for device in devices:
-        connection = connect_to_device(device)
-        connections.append(connection)
+        device['username'] = user_name
+        device['password'] = pass_word
+        device["connection"] = connect_to_device(device)
+        device.pop("password")
+        device.pop("username")
+        connections.append(device)
     yield connections
-    for connection in connections:
-        connection.disconnect()
+    for device in connections:
+        device["connection"].disconnect()
