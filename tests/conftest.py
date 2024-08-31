c6be9ba409fe0940cfa33ed16ea400e07309cad2
Dipankar Shaw <dipankarshaw@microsoft.com>
2024-09-01 01:38:32 +0530

    added parallism to the code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from src.device_connector import load_device_config, connect_to_device
+from concurrent.futures import ThreadPoolExecutor
 
 @pytest.fixture(scope="module")
 def device_connections():
@@ -8,13 +9,23 @@ def device_connections():
     connections = []
     user_name = os.getenv('USERNAME')
     pass_word = os.getenv('PASSWORD')
-    for device in devices:
-        device['username'] = user_name
-        device['password'] = pass_word
-        device["connection"] = connect_to_device(device)
-        device.pop("password")
-        device.pop("username")
-        connections.append(device)
+    
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for device in devices:
+            device['username'] = user_name
+            device['password'] = pass_word
+            futures.append(executor.submit(connect_to_device, device))
+        
+        for future in futures:
+            connection = future.result()
+            device = connection.get_device()
+            device.pop("password")
+            device.pop("username")
+            device["connection"] = connection
+            connections.append(device)
+    
     yield connections
+    
     for device in connections:
         device["connection"].disconnect()
