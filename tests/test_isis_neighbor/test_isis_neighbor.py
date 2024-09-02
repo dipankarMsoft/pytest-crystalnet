c23d3ef840ab202111b3d6986f70413e0bff4af2
Dipankar Shaw <dipankarshaw@microsoft.com>
2024-09-02 11:44:19 +0530

    writing the dicts in a Yaml file.

--- tests/test_isis_neighbor/test_isis_neighbor.py
+++ tests/test_isis_neighbor/test_isis_neighbor.py
@@ -10,30 +10,59 @@ def load_yaml_file(path):
     
 isis_command = {'cisco_xr': 'show isis neighbors', 'arista_eos': 'show isis neighbors', 'juniper_junos': 'show isis adjacency'}
 
-def make_neighbor_dict(matches):
+def make_neighbor_dict_juniper_junos(matches):
+    neighbor_dict = {}
+    for match in matches:
+        system_id = match[1]
+        state = match[2]
+        if_name = match[0]
+        neighbor_dict[system_id] = {'if_name': if_name, 'state': state}
+    return neighbor_dict
+
+def make_neighbor_dict_arista_eos(matches):
     neighbor_dict = {}
     for match in matches:
         system_id = match[0]
-        state = match[1]
-        neighbor_dict[system_id] = state
+        if_name = match[1]
+        state = match[2]
+    neighbor_dict[system_id] = {'if_name': if_name, 'state': state}
     return neighbor_dict
+
+def make_neighbor_dict_cisco_xr(matches):
+    neighbor_dict = {}
+    for match in matches:
+        system_id = match[0]
+        if_name = match[1]
+        state = match[2]
+        neighbor_dict[system_id] = {'if_name': if_name, 'state': state}
+    return neighbor_dict
+
+# Convert the dictionary to YAML and write it to a file
+def write_dict_to_yaml(dictionary, path):
+    with open(path, 'w') as file:
+        yaml.dump(dictionary, file)
+
 def test_isis_adj_data(device_connections):
     isis_neighbor_data = load_yaml_file(path="tests/test_isis_neighbor/expected_isis_neighbors.yaml")
-    cisco_xr_pattern = r'(\w+\.\w+)\s+\w+\s+\*\w+\*\s+(Up|Down)'
-    arista_eos_pattern = r'\S+\s+(\w+\.\w+)\s+\S+\s+\S+\s+\S+\s+(\w+)'
-    juniper_junos_pattern = r'\S+\.\S+\s+(\w+\.\w+)\s+\S+\s+(\w+)'
+    juniper_junos_pattern = r'(\S+\.\S+)\s+(\w+\.\w+)\s+\S+\s+(\w+)'
+    arista_eos_pattern = r'\S+\s+(\w+\.\w+)\s+\S+\s+(\S+)\s+\S+\s+(\w+)'
+    cisco_xr_pattern = r'(\w+\.\w+)\s+(\w+)\s+\*\w+\*\s+(Up|Down)'
+
     for device in device_connections:
         output = device["connection"].send_command(isis_command.get(device["device_type"]))
         expected_isis_neighbor = isis_neighbor_data.get(device["host"])
         if device["device_type"] == "cisco_xr":
             matches = re.findall(cisco_xr_pattern, output)
-            neighbor_dict = make_neighbor_dict(matches)
+            neighbor_dict = make_neighbor_dict_cisco_xr(matches)
+            write_dict_to_yaml(neighbor_dict, f"{device['host']}_isis_neighbors.yaml")
         elif device["device_type"] == "arista_eos":
             matches = re.findall(arista_eos_pattern, output)
-            neighbor_dict = make_neighbor_dict(matches)
+            neighbor_dict = make_neighbor_dict_arista_eos(matches)
+            write_dict_to_yaml(neighbor_dict, f"{device['host']}_isis_neighbors.yaml")
         elif device["device_type"] == "juniper_junos":
             matches = re.findall(juniper_junos_pattern, output)
-            neighbor_dict = make_neighbor_dict(matches)
+            neighbor_dict = make_neighbor_dict_juniper_junos(matches)
+            write_dict_to_yaml(neighbor_dict, f"{device['host']}_isis_neighbors.yaml")
         print(neighbor_dict)
         for neighbor in expected_isis_neighbor:
-            assert neighbor_dict.get(neighbor) in ['Up','UP'], f"Expected neighbor {neighbor} state to be Up, but got {neighbor_dict.get(neighbor)}"
+            assert neighbor_dict.get(neighbor['state']) in ['Up','UP'], f"Expected neighbor {neighbor} state to be Up, but got {neighbor_dict.get(neighbor['state'])}"
