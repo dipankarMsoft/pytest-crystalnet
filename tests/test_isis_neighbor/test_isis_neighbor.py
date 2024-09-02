c901df7785ee6802c3f858b04b2625b4ed1e5703
Dipankar Shaw <dipankarshaw@microsoft.com>
2024-09-02 10:10:30 +0530

    added isis test Data

---
+++ tests/test_isis_neighbor/test_isis_neighbor.py
@@ -0,0 +1,20 @@
+
+import re
+import yaml
+import pytest
+
+
+def load_yaml_file(path):
+    with open(path) as file:
+        return yaml.safe_load(file)
+    
+isis_command = {'cisco_xr': 'show isis neighbors', 'arista_eos': 'show isis neighbors', 'juniper_junos': 'show isis adjacency'}
+
+def test_device_version(device_connections):
+    isis_neighbor_data = load_yaml_file(path="tests/test_isis_neighbor/expected_isis_neighbors.yaml")
+    for device in device_connections:
+        output = device["connection"].send_command(isis_command.get(device["device_type"]))
+        print(output)
+        expected_isis_neighbor = isis_neighbor_data.get(device["host"])
+        for neighbor in expected_isis_neighbor:
+            assert neighbor in output, f"Expected neighbor {neighbor}, but got {output}"
--- tests/test_version/test_device_version.py
+++ tests/test_version/test_device_version.py
@@ -3,19 +3,14 @@ import re
 import yaml
 import pytest
 
-device_pattern = {'cisco_xr': 'Version\s+:\s(\S+)\s', 'arista_eos': 'Software image version:\s(\S+)', 'juniper_junos': 'Junos:\s(\S+)'}
 
 def load_device_versions(path):
     with open(path) as file:
         return yaml.safe_load(file)
 
 def test_device_version(device_connections):
-    device_versions = load_device_versions(path="tests/device_versions.yaml")
-    print(device_pattern)
+    device_versions = load_device_versions(path="tests/test_version/device_versions.yaml")
     for device in device_connections:
         output = device["connection"].send_command("show version")
-        match = re.match(device_pattern.get(device["device_type"]), output, re.MULTILINE)
-        if match:
-            print(match.group(1))
         expected_version = device_versions.get(device["device_type"])
         assert expected_version in output, f"Expected version {expected_version}, but got {output}"
